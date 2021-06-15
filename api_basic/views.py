c1415ee1dfcc293c81a7d2405e143ffd8276b6ee
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-15 23:03:12 +0530

    created a request to get all the articles in Json and to post the article

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,23 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        parsed_request = JSONParser().parse(request)
+        serializer = ArticleSerializer(parsed_request)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=401)
+    
