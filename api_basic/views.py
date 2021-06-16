4e6765825dab16ac44d63b1eb71e23fd4ce6cf20
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-16 13:16:10 +0530

    added api view for easy dealing with Apis

--- api_basic/views.py
+++ api_basic/views.py
@@ -6,23 +6,23 @@ from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        parsed_request = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=parsed_request)
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=401)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 def article_detail(request, pk):
